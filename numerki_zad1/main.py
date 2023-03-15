d7fa585fc80c8469337eff4121ddeb29b3928461
Jakub Łysiuk <242457@edu.p.lodz.pl>
2023-03-15 20:50:45 +0100

    bisection and falsi done

--- numerki_zad1/main.py
+++ numerki_zad1/main.py
@@ -1,11 +1,15 @@
 import front
+import regula_falsi
 
-# f = lambda x: -5*x**4 + 2*x**2 + 3*x +12
+f = lambda x: -5*x**4 + 2*x**2 + 3*x + 12
 #
 # print(bisections.bisection_tolerance_poly([12, 3, 2, 0, -5], 0.1, 3.2, 0.001))
 # print(bisections.bisection_iteration_poly([12, 3, 2, 0, -5], 0.1, 3.2, 100))
 # print(regula_falsi.falsi_tolerance(f, 0.1, 3.2, 0.001))
-
+print(regula_falsi.falsi_iteration(f, 0.1, 3.2, 66))
+print(regula_falsi.falsi_iteration_poly([12, 3, 2, 0, -5], 0.1, 3.2, 66))
+print(regula_falsi.falsi_tolerance(f, 0.1, 3.2, 0.001))
+print(regula_falsi.falsi_tolerance_poly([12, 3, 2, 0, -5], 0.1, 3.2, 0.001))
 # print(g(2.0))
 # print(horner.horner_result([-2, 3, 2, -7.5], 2.0))
 
--- numerki_zad1/regula_falsi.py
+++ numerki_zad1/regula_falsi.py
@@ -1,5 +1,7 @@
 from typing import Tuple, Any
 
+import horner
+
 
 def falsi_tolerance(f, a, b, d, count=0) -> int | tuple[Any, int | Any]:
     """
@@ -12,8 +14,10 @@ def falsi_tolerance(f, a, b, d, count=0) -> int | tuple[Any, int | Any]:
     :return:
     """
     if f(a) * f(b) >= 0:
-        print("You have not assumed right a and b")
-        return -1
+        print('wartości na początku i końcu przedziału są tych samych znaków. Wprowadź inne parametry.')
+        aa = float(input('Podaj Parametr a: '))
+        bb = float(input('Podaj Parametr b: '))
+        falsi_tolerance(f, aa, bb, d)
 
     c = a  # Initialize result
 
@@ -34,12 +38,88 @@ def falsi_tolerance(f, a, b, d, count=0) -> int | tuple[Any, int | Any]:
             a = c
 
 
-# def falsi_iteration(f, a: float, b: float, i: int) -> float:
-#     """
-#     :param f:
-#     :param a:
-#     :param b:
-#     :param i:
-#     :return:
-#     """
+def falsi_tolerance_poly(tab, a, b, d, count=0) -> int | tuple[Any, int | Any]:
+    """
+    wyznacza miejsce zerowe funkcji za pomocą Regula falsi
+    :param tab: tablica współczynników wielomianu
+    :param a: dolna granica przedziału izolacji
+    :param b: górna granica przedziału izolacji
+    :param d: tolerancja
+    :param count: licznik iteracji klauzuli while
+    :return:
+    """
+    if horner.horner_result(tab, a) * horner.horner_result(tab, b) >= 0:
+        print('wartości na początku i końcu przedziału są tych samych znaków. Wprowadź inne parametry.')
+        aa = float(input('Podaj Parametr a: '))
+        bb = float(input('Podaj Parametr b: '))
+        falsi_iteration_poly(tab, aa, bb, d)
 
+    c = a  # Initialize result
+
+    while True:
+        count = count + 1
+        # szukamy miejsca zerowego fałszywej prostej
+        c = (a * horner.horner_result(tab, b) - b * horner.horner_result(tab, a)) / (
+                    horner.horner_result(tab, b) - horner.horner_result(tab, a))
+
+        # sprawdzamy czy x0 fałszywej prostej to x0 funkcji
+        if abs(horner.horner_result(tab, c)) < d:
+            return c, count
+
+        # wybieramy jedną połowę przedziału aby kontynuować
+        elif (horner.horner_result(tab, c) * horner.horner_result(tab, a)) < 0:
+            b = c
+
+        else:
+            a = c
+
+
+def falsi_iteration(f, a: float, b: float, i: int) -> float:
+    """
+    :param f: wzór funkcji
+    :param a: dolna granica przedziału izolacji
+    :param b: górna granica przedziału izolacji
+    :param i: liczba iteracji
+    :return:
+    """
+    if f(a) * f(b) >= 0:
+        print('wartości na początku i końcu przedziału są tych samych znaków. Wprowadź inne parametry.')
+        aa = float(input('Podaj Parametr a: '))
+        bb = float(input('Podaj Parametr b: '))
+        falsi_iteration(f, aa, bb, i)
+    c = a
+    for i in range(i):
+        # szukamy miejsca zerowego fałszywej prostej
+        c = (a * f(b) - b * f(a)) / (f(b) - f(a))
+        # wybieramy jedną połowę przedziału aby kontynuować
+        if (f(c) * f(a)) < 0:
+            b = c
+        else:
+            a = c
+    return c
+
+
+def falsi_iteration_poly(tab, a: float, b: float, i: int) -> float:
+    """
+    :param tab: tablica współczynników wielomianu
+    :param a: dolna granica przedziału izolacji
+    :param b: górna granica przedziału izolacji
+    :param i: liczba iteracji
+    :return:
+    """
+    if horner.horner_result(tab, a) * horner.horner_result(tab, b) >= 0:
+        print('wartości na początku i końcu przedziału są tych samych znaków. Wprowadź inne parametry.')
+        aa = float(input('Podaj Parametr a: '))
+        bb = float(input('Podaj Parametr b: '))
+        falsi_iteration_poly(tab, aa, bb, i)
+    c = a
+    for i in range(i):
+        # szukamy miejsca zerowego fałszywej prostej
+        c = (a * horner.horner_result(tab, b) - b * horner.horner_result(tab, a)) / (
+                horner.horner_result(tab, b) - horner.horner_result(tab, a))
+        # wybieramy jedną połowę przedziału aby kontynuować
+        if (horner.horner_result(tab, c) * horner.horner_result(tab, a)) < 0:
+            b = c
+        else:
+            a = c
+    return c
